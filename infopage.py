ac06bf08cbdbac4dceea779266f4911b36e5d93f
phdguy087 <130491535+phdguy087@users.noreply.github.com>
2024-03-06 15:04:08 -0500

    Update infopage.py

--- infopage.py
+++ infopage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 def run():
-    iframe_src = "src="https://cosspp.maps.arcgis.com/apps/webappviewer/index.html?id=7fa44a0704c04506b169f7abf077f810"
+    iframe_src = "src=https://cosspp.maps.arcgis.com/apps/webappviewer/index.html?id=7fa44a0704c04506b169f7abf077f810"
     components.iframe(iframe_src)
 if __name__ == "__main__":
     run()
