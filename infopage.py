58ede90c3cfb22a0982019b15c9dafef9ee3f682
phdguy087 <130491535+phdguy087@users.noreply.github.com>
2024-03-06 15:45:17 -0500

    Update infopage.py

--- infopage.py
+++ infopage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 def run():
     iframe_src = "https://cosspp.maps.arcgis.com/apps/webappviewer/index.html?id=7fa44a0704c04506b169f7abf077f810"
-    components.iframe(iframe_src,height=800, width = 1200)
+    components.iframe(iframe_src,height=800, width = 1200,loc="center")
 
 st.set_page_config(layout="wide")
 st.title("iPlantGreenS\u00b2 : Integrated Planning toolkit for Green Infrastructure Siting and Selection")
