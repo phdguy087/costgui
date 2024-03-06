0faff72cfec93fd0be2c5434392c008481bdf60b
phdguy087 <130491535+phdguy087@users.noreply.github.com>
2024-03-06 14:56:36 -0500

    Update infopage.py

--- infopage.py
+++ infopage.py
@@ -1,6 +1,10 @@
 import streamlit as st
-<iframe
-  src="https://cosspp.maps.arcgis.com/apps/webappviewer/index.html?id=7fa44a0704c04506b169f7abf077f810/?embed=true"
-  height="450"
-  style={{ width: "100%", border: "none" }}
-></iframe>
+st.markdown(
+    """
+    <iframe
+      src="https://cosspp.maps.arcgis.com/apps/webappviewer/index.html?id=7fa44a0704c04506b169f7abf077f810/?embed=true"
+      height="450"
+      style={{ width: "100%", border: "none" }}
+    ></iframe>
+    """
+)
